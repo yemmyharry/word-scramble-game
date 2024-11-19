a02eadd74ea1f6d803c4114392cc5067474f5069
yemmyharry <yemmyharry@gmail.com>
2024-11-19 17:20:21 +0100

    Add word list data source

---
+++ src/data/words.ts
@@ -0,0 +1,12 @@
+export const WORDS: string[] = [
+    "react",
+    "javascript",
+    "typescript",
+    "programming",
+    "developer",
+    "component",
+    "state",
+    "props",
+    "redux",
+    "router",
+  ];
